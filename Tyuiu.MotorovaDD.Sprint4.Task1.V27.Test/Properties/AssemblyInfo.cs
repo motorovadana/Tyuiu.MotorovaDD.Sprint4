a660e8a206d7a5c8991df41b20e9c0c28925b688
Tyuiu.MotorovaDD@outlook.com <Tyuiu.MotorovaDD@outlook.com>
2023-11-05 01:10:18 +0500

    Task1

---
+++ Tyuiu.MotorovaDD.Sprint4.Task1.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MotorovaDD.Sprint4.Task1.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.MotorovaDD.Sprint4.Task1.V27.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58483663-723f-4ca0-aee4-1bba58a3199a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
